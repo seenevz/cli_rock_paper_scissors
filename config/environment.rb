b156d2ea284ca9cadadf9be16108b568891a74aa
Sergio Neves <sernevess@gmail.com>
2019-06-28 12:56:34 +0100

    add cli

--- config/environment.rb
+++ config/environment.rb
@@ -2,11 +2,15 @@ require "require_all"
 require "http"
 require "json"
 require "pry"
-require 'bundler'
+require "bundler"
+require "tty"
+require "tty-box"
+require "tty-screen"
+
 Bundler.require
 
-require_relative '../lib/cli.rb'
-require_all 'dev'
+require_relative "../lib/cli.rb"
+require_all "dev"
 
 ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: "db/development.db")
 
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,4 +1,24 @@
 require_all "lib"
 
-class CLI
+@outer_box = lambda {
+  TTY::Box.frame(
+    width: (TTY::Screen.width),
+    height: ((TTY::Screen.height) / 2),
+    align: :center,
+    padding: 1,
+  ) do
+    "Drawing a box in terminal emulator"
+  end
+}
+
+def screen_box(box1)
+  TTY::Box.frame(
+    width: (TTY::Screen.width),
+    height: (TTY::Screen.height),
+    align: :center,
+    padding: 3,
+  ) do
+    binding.pry
+    print box1.call
+  end
 end
